c7f2a7a4d66aabe0ace0f1b454b1ed580459795d
SeungHyun <fm10033@gmail.com>
2021-07-26 18:55:26 +0900

    onChange, onCreatem CreateUser.js 구현

--- src/App.js
+++ src/App.js
@@ -1,37 +1,67 @@
 
-import React, {useRef}from "react";
+import React, { useRef, useState } from "react";
+import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username:'',
+    email:''
+  })
+  const {username, email} = inputs
+  const [users,setUsers]  = useState([
     {
-        id: 1,
-        username: 'herrakam',
-        email: 'fm1003@naver.com',
-        active: true,
+      id: 1,
+      username: 'herrakam',
+      email: 'fm1003@naver.com',
     },
     {
-        id: 2,
-        username: 'herrakam2',
-        email: 'fm10033@naver.com',
-        active: false,
+      id: 2,
+      username: 'herrakam2',
+      email: 'fm10033@naver.com',
     },
     {
-        id: 3,
-        username: 'herrakam3',
-        email: 'fm100333@naver.com',
-        active: false,
+      id: 3,
+      username: 'herrakam3',
+      email: 'fm100333@naver.com',
     }
-]
+  ])
 
-const nextid = useRef(4)
+  const nextId = useRef(4)
+  // const onChange = e => {
+  //   const { name, value } = e.terget
+  //   setInputs({
+  //     ...inputs,
+  //     [name]: value,
+  //   })
+  // }
+  const onChange =  e => {
+    const { name, value } = e.target
+    setInputs({
+      ...inputs,
+      [name]: value,
+    })
+  }
+  const onCreate = ()=>{
+    const user = {
+      id:nextId.current,
+      username,
+      email
+    }
+    setUsers(users=>users.concat(user))
+    setInputs({
+      ...inputs,
+      username:'',
+      email:''
+    })
+  }
 
-const onCreate = ()=>{
-  nextid.current += 1
-}
+
+  
   return (
     <>
-      <UserList users = {users}></UserList>
+      <CreateUser onChange = {onChange} username={username} onCreate={onCreate} email = {email}></CreateUser>
+      <UserList users={users}></UserList>
     </>
   )
 }
--- src/CreateUser.js
+++ src/CreateUser.js
@@ -1,12 +1,13 @@
 import React from 'react'
 
-function CreateUser ({username, email, onChange, onCreate} ){
-return(
-    <>
-    <input placeholder='계정' name='username' value={username} onChange={onChange}/>
-    <input placeholder='이메일' name='email' value={email} onChange={onChange}></input>
-    <button onClick={onCreate} >등록</button>
-    </>
-)
+function CreateUser({username, email, onChange, onCreate}){
+    return (
+        <div>
+        <input type="text" placeholder='이름' name='username' value = {username} onChange={onChange} />
+        <input type="text" placeholder='계정' name='email' value = {email} onChange={onChange} />
+        <button onClick={onCreate}>등록</button>
+        </div>
+        )
 }
+
 export default CreateUser
