c7f9248dad0e65988bb51916379a41fd08efbe60
SeungHyun <fm10033@gmail.com>
2021-07-27 17:45:07 +0900

    onToggle 구현

--- src/App.js
+++ src/App.js
@@ -5,59 +5,69 @@ import UserList from "./UserList";
 
 function App() {
   const [inputs, setInputs] = useState({
-    username:'',
-    email:''
+    username: '',
+    email: ''
   })
-  const {username, email} = inputs
-  const [users,setUsers]  = useState([
+  const { username, email } = inputs
+  const [users, setUsers] = useState([
     {
       id: 1,
       username: 'herrakam',
       email: 'fm1003@naver.com',
+      active: true
     },
     {
       id: 2,
       username: 'herrakam2',
       email: 'fm10033@naver.com',
+      active: true
     },
     {
       id: 3,
       username: 'herrakam3',
       email: 'fm100333@naver.com',
+      active: false
     }
   ])
 
   const nextId = useRef(4)
-  const onChange =  e => {
+  const onChange = e => {
     const { name, value } = e.target
     setInputs({
       ...inputs,
       [name]: value,
     })
   }
-  const onCreate = ()=>{
+  const onCreate = () => {
     const user = {
-      id:nextId.current,
+      id: nextId.current,
       username,
       email
     }
     setUsers(users.concat(user))
     setInputs({
       ...inputs,
-      username:'',
-      email:''
+      username: '',
+      email: ''
     })
   }
-  const onRemove = (id)=>{
-    setUsers(users.filter(user=>user.id!==id))
+  const onRemove = (id) => {
+    setUsers(users.filter(user => user.id !== id))
+  }
+
+  const onToggle = id=>{
+    setUsers(users.map(user=>user.id===id?
+      {...user, active : !user.active}
+      : user
+      ))
   }
 
 
-  
+
   return (
     <>
-      <CreateUser onChange = {onChange} username={username} onCreate={onCreate} email = {email}></CreateUser>
-      <UserList users={users} onRemove={onRemove}></UserList>
+      <CreateUser onChange={onChange} username={username} onCreate={onCreate} email={email}></CreateUser>
+      <UserList users={users} onRemove={onRemove} onToggle={onToggle}></UserList>
     </>
   )
 }
--- src/UserList.js
+++ src/UserList.js
@@ -1,20 +1,23 @@
 import React from 'react'
 
-function User({ user, onRemove }) {
-    const {username, email, id} = user
+function User({ user, onRemove,onToggle}) {
+    const {username, email, id, active} = user
     return (
         <div className="user">
-            <b>{username}</b><span>{email}</span><button onClick={()=>onRemove(id)}>제거</button>
+            <b style={{
+                color: active ? 'blue' : 'black',
+                cursor: 'pointer'
+            }} onClick={()=>{onToggle(id)}}>{username}</b><span>{email}</span><button onClick={()=>onRemove(id)}>제거</button>
         </div>
     )
 }
 
 
-function UserList({users, onRemove}) {
+function UserList({users, onRemove, onToggle}) {
 
     return (
     <>
-         {users.map(user => <User user={user} key={user.id} onRemove={onRemove}/>)}         
+         {users.map(user => <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle} />)}         
     </>
 
             )
